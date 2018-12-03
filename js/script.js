87b2b4555e43c9d1ecc706cf78be6bfc280dbc1d
Taras Rushchak <taraskhust@gmail.com>
2018-12-03 10:05:45 +0200

    tabs done

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-window.addEventListener('DOMContentLoaded', () => {
+(window.addEventListener('DOMContentLoaded', () => {
 
     'use strict';
     let tab = document.querySelectorAll('.info-header-tab'),
@@ -76,4 +76,4 @@ window.addEventListener('DOMContentLoaded', () => {
 
     object.hideTabContent(1);
     object.addEvent();
-});
+}));
